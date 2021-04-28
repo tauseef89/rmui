6df9ca0caa9296216c6875494b2680e6e25621c7
tauseef89 <tauseef.n89@gmail.com>
2021-04-28 14:32:46 +0530

    part 6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Blogs from './pages/Blogs'
 import Create from './pages/Create'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import { purple } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Blogs />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Blogs />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,17 +6,7 @@ import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover': {
-      background: 'blue'
-    },
-  },
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20,
-  }
+
 })
 
 export default function Create() {
@@ -38,6 +28,7 @@ export default function Create() {
         className={classes.btn}
         onClick={() => console.log('you clicked me')}
         type="submit" 
+        //color="primary" 
         color="secondary" 
         variant="contained"
         endIcon={<KeyboardArrowRightIcon />}>
