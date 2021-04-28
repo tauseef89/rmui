de531f449552614b5d03e3b523c5996c6e826f80
tauseef89 <tauseef.n89@gmail.com>
2021-04-28 13:33:38 +0530

    stater files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Blogs from './pages/Blogs'
+import Create from './pages/Create'
+
 function App() {
   return (
-    <div className="App">
-      demo app
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Blogs />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
