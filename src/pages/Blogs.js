2f147b4e1167b8ef400d91753c8431d9a6357086
tauseef89 <tauseef.n89@gmail.com>
2021-05-03 21:34:15 +0530

    part 10

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 
 export default function Blogs() {
   const [blogs, setBlogs] = useState([]);
@@ -10,10 +13,28 @@ export default function Blogs() {
   }, [])
 
   return (
-    <div>
-      {blogs.map(blog => (
-        <p key={blog.id}>{ blog.title }</p>
-      ))}
-    </div>
+    <Container>
+      {/* <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+      <Grid container spacing={3}>
+        {blogs.map(blog => (
+          <Grid item xs={12} md={6} lg={4} key={blog.id}>
+            <Paper>{blog.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>    
   )
 }
